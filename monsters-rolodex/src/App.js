dac50257e5c213e432ad6203584c228e114f98f4
Abhishek Singh <as9284608@gmail.com>
2022-11-12 20:12:59 +0530

    searcing and filtering

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -4,12 +4,14 @@ import { Component } from "react";
 class App extends Component {
   constructor() {
     super();
+    console.log("constructor");
     this.state = {
       monsters: [],
     };
   }
 
   componentDidMount() {
+    console.log("componentDidMount");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) =>
@@ -25,8 +27,27 @@ class App extends Component {
   }
 
   render() {
+    console.log("render");
     return (
       <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return {
+                monsters: filteredMonsters,
+              };
+            });
+          }}
+        />
         {this.state.monsters.map((monster) => {
           return <h1 key={monster.id}>{monster.name}</h1>;
         })}
